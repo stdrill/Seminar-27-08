e0ffb641c32cac32f69ba4580c82300b88fc54fd
stdrill <tanya23.96@mail.ru>
2022-08-29 22:11:33 +0300

    task3 added

---
+++ task3/Program.cs
@@ -0,0 +1,14 @@
+﻿// Определить количество цифр в числе
+
+int Quantity(int number)
+{
+    int i = 1;
+    while ((number/10) !=0){
+        number = number / 10;
+        i++;
+    }
+    return i;
+}
+Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"{Quantity(a)}");
