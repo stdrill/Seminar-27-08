858d3007d79e68bc5c9aa06d2318fabd82c95094
stdrill <tanya23.96@mail.ru>
2022-08-29 21:15:14 +0300

    task1 added

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Показать таблицу квадратов чисел от 1 до N
+
+void FillNumber(int number1)
+{
+    int count = 1;
+    while (count <= number1) 
+    {
+        Console.WriteLine($"{count} * {count} = {count * count}");
+        count++;
+    } 
+}
+Console.WriteLine("Введите число N: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+FillNumber(number);
